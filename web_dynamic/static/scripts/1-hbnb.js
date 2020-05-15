7b0e4decad76f0d3791648b0c5b0d446c1e7cf0a
shincap8 <shincap8@gmail.com>
2020-05-15 18:23:31 +0000

    Id and name in html and  to this in js

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -3,14 +3,16 @@ $(function () {
   const amenitiesDict = {};
   $('.amenities ul li input').change(function () {
     if ($(this).is(':checked')) {
-      console.log(this.attr('data-name'));
-      // amenitiesNames.push(this.attr('data-name'));
-      // amenitiesDict[this.attr('data-id')] = this.attr('data-name');
-    } /* else {
-      const pos = amenitiesNames.indexOf(this.attr('data-name'));
+      amenitiesNames.push($(this).attr('data-name'));
+      amenitiesDict[$(this).attr('data-id')] = $(this).attr('data-name');
+      console.log(amenitiesNames);
+      console.log(amenitiesDict);
+    } else {
+      const pos = amenitiesNames.indexOf($(this).attr('data-name'));
       amenitiesNames.splice(pos, 1);
-      amenitiesDict.remove(this.attr('data-id'));
-    }
-    $('.amenities h4').text(amenitiesNames.join()); */
+      console.log()
+      delete amenitiesDict.$(this).attr('data-id');
+    } 
+    $('.amenities h4').text(amenitiesNames.join(', '));
   });
 });
