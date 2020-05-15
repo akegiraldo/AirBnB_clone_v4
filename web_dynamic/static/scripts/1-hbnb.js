456a5f1c68aaeb4e30f755c40e4181250b7b06ef
Doouh <ake.giraldo@hotmail.com>
2020-05-15 13:46:02 -0500

    Fix checkbox bug, add delete from dict

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -5,14 +5,16 @@ $(function () {
     if ($(this).is(':checked')) {
       amenitiesNames.push($(this).attr('data-name'));
       amenitiesDict[$(this).attr('data-id')] = $(this).attr('data-name');
-      console.log(amenitiesNames);
-      console.log(amenitiesDict);
     } else {
       const pos = amenitiesNames.indexOf($(this).attr('data-name'));
       amenitiesNames.splice(pos, 1);
-      console.log()
-      delete amenitiesDict.$(this).attr('data-id');
-    } 
-    $('.amenities h4').text(amenitiesNames.join(', '));
+      const id = $(this).attr('data-id');
+      delete amenitiesDict.id;
+    }
+    if (amenitiesNames.length === 0) {
+      $('.amenities h4').html('&nbsp;');
+    } else {
+      $('.amenities h4').text(amenitiesNames.join(', '));
+    }
   });
 });
