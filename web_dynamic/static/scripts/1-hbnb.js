f045bf822cdee686f46d16738d45f7008abe377c
shincap8 <shincap8@gmail.com>
2020-05-15 17:13:39 +0000

    check attr

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,15 +1,16 @@
 $(function () {
-  let amenities_names = [];
-  let amenities_dict = {}
+  const amenitiesNames = [];
+  const amenitiesDict = {};
   $('.amenities ul li input').change(function () {
     if ($(this).is(':checked')) {
-      amenities_names.push(this.attr('data-name'));
-      amenities_dict[this.attr('data-id')] = this.attr('data-name');
-    } else {
-      let pos = amenities_names.indexOf(this.attr('data-name'));
-      amenities_names.splice(pos, 1);
-      amenities_dict.remove(this.attr('data-id'));
+      console.log(this.attr('data-name'));
+      // amenitiesNames.push(this.attr('data-name'));
+      // amenitiesDict[this.attr('data-id')] = this.attr('data-name');
+    } /* else {
+      const pos = amenitiesNames.indexOf(this.attr('data-name'));
+      amenitiesNames.splice(pos, 1);
+      amenitiesDict.remove(this.attr('data-id'));
     }
-    $('.amenities h4').text(amenities_names.join());
-  })
+    $('.amenities h4').text(amenitiesNames.join()); */
+  });
 });
