cda94f8965501e0514bd8e8a19a6d20421242d22
Doouh <ake.giraldo@hotmail.com>
2020-05-16 11:51:03 -0500

    Show places dynamically

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -10,13 +10,45 @@ $(function () {
   $.ajax({
     url: 'http://0.0.0.0:5001/api/v1/places_search/',
     type: 'post',
-    data: {},
+    data: '{}',
     headers: {
       'Content-Type': 'application/json'
     },
     dataType: 'json'
-  }).done(function (data) {
-    console.info(data);
+  }).done(function (places) {
+    console.log(places);
+    /*function compare( placeA, placeB ) {
+      if ( placeA.name < placeB.name ){
+        return -1;
+      }
+      if ( placeB.name > placeB.name ){
+        return 1;
+      }
+      return 0;
+    }
+    console.log(places.sort(compare));*/
+    for (const place of places) {
+      $('.places').append(`
+        <article>
+	        <div class="title_box">
+	          <h2>${place.name}</h2>
+	          <div class="price_by_night">${place.price_by_night}</div>
+	        </div>
+	        <div class="information">
+            <div class="max_guest">${place.max_guest} Guests</div>
+            <div class="number_rooms">${place.number_rooms} Bedrooms</div>
+            <div class="number_bathrooms">${place.number_bathrooms} Bathroom</div>
+	        </div>
+	        <div class="user">
+            <b>Owner:</b> ${place.user_id}
+          </div>
+          <div class="description">
+	          ${place.description}
+          </div>
+	      </article>`);
+    }
+  }).fail(function (e) {
+    console.log(e);
   });
 
   $('.amenities ul li input').change(function () {
